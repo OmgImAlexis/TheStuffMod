301d57ba09afa8ad685b1c595fc60e9235403218
Alexis Tyler <OmgImAlexis@gmail.com>
2014-09-15 18:21:28 +0930

    Changed stuff?

--- src/main/java/omgimalexis/allthethings/block/Blocks.java
+++ src/main/java/omgimalexis/allthethings/block/Blocks.java
@@ -6,7 +6,6 @@ import omgimalexis.allthethings.creativetabs.TMCreativeTabs;
 import omgimalexis.allthethings.lib.Strings;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-
 public class Blocks extends net.minecraft.init.Blocks {
 
 	public static void mainRegistry() {
@@ -18,14 +17,19 @@ public class Blocks extends net.minecraft.init.Blocks {
 	public static Block tutBlock;
 	public static Block tutCrop;
 	public static Block multiSided;
-	
+
 	// Ores
 	public static Block adamanite_ore;
+	public static Block amathyst_ore;
 	public static Block blurite_ore;
+	public static Block carnelian_ore;
+	public static Block citrine_ore;
 	public static Block copper_ore;
 	public static Block daeyalt_ore;
+	public static Block jade_ore;
 	public static Block lunar_ore;
 	public static Block mithril_ore;
+	public static Block onyx_ore;
 	public static Block rubium_ore;
 	public static Block ruby_ore;
 	public static Block runite_ore;
@@ -33,12 +37,13 @@ public class Blocks extends net.minecraft.init.Blocks {
 	public static Block silver_ore;
 	public static Block tin_ore;
 	public static Block topaz_ore;
-	
+
 	// Other blocks
+	public static Block amber;
 	public static Block granite;
 	public static Block limestone;
 	public static Block opal;
-	
+
 	// Compressed blocks
 	public static Block compressed_iron_block;
 	public static Block compressed_gold_block;
@@ -46,11 +51,11 @@ public class Blocks extends net.minecraft.init.Blocks {
 	public static Block compressed_ruby_block;
 	public static Block compressed_sapphire_block;
 	public static Block compressed_diamond_block;
-	
+
 	// Blocks
 	public static Block ruby_block;
 	public static Block sapphire_block;
-	
+
 	// Machines
 	public static Block dark_matter_furnace;
 	public static Block dark_matter_furnace_active;
@@ -60,7 +65,7 @@ public class Blocks extends net.minecraft.init.Blocks {
 		tutBlock = new BlockBigBang().setBlockName("TutBlock").setCreativeTab(TMCreativeTabs.tabBlock);
 		multiSided = new MultiSided(Material.ground).setBlockName("MultiSided").setCreativeTab(TMCreativeTabs.tabBlock);
 		tutCrop = new TutCrop().setBlockName("TutCrop").setCreativeTab(TMCreativeTabs.tabBlock).setBlockTextureName(Strings.MODID + ":TutCrop");
-	
+
 		// Ores
 		adamanite_ore = new BlockOre().setBlockName("adamanite_ore");
 		blurite_ore = new BlockOre().setBlockName("blurite_ore");
@@ -75,12 +80,12 @@ public class Blocks extends net.minecraft.init.Blocks {
 		silver_ore = new BlockOre().setBlockName("silver_ore");
 		tin_ore = new BlockOre().setBlockName("tin_ore");
 		topaz_ore = new BlockOre().setBlockName("topaz_ore");
-		
+
 		// Other blocks
 		granite = new BlockOre().setBlockName("granite");
 		limestone = new BlockOre().setBlockName("limestone");
 		opal = new BlockOre().setBlockName("opal");
-		
+
 		// Compressed blocks
 		compressed_iron_block = new BlockOre().setBlockName("compressed_iron_block");
 		compressed_gold_block = new BlockOre().setBlockName("compressed_gold_block");
@@ -88,11 +93,11 @@ public class Blocks extends net.minecraft.init.Blocks {
 		compressed_ruby_block = new BlockOre().setBlockName("compressed_ruby_block");
 		compressed_sapphire_block = new BlockOre().setBlockName("compressed_sapphire_block");
 		compressed_diamond_block = new BlockOre().setBlockName("compressed_diamond_block");
-		
+
 		// Blocks
 		ruby_block = new BlockOre().setBlockName("ruby_block");
 		sapphire_block = new BlockOre().setBlockName("sapphire_block");
-		
+
 		// Machines
 		dark_matter_furnace = new DarkMatterFurnace(false).setBlockName("dark_matter_furnace");
 		dark_matter_furnace_active = new DarkMatterFurnace(true);
@@ -118,12 +123,12 @@ public class Blocks extends net.minecraft.init.Blocks {
 		GameRegistry.registerBlock(silver_ore, silver_ore.getUnlocalizedName());
 		GameRegistry.registerBlock(tin_ore, tin_ore.getUnlocalizedName());
 		GameRegistry.registerBlock(topaz_ore, topaz_ore.getUnlocalizedName());
-		
+
 		// Other blocks
 		GameRegistry.registerBlock(granite, granite.getUnlocalizedName());
 		GameRegistry.registerBlock(limestone, limestone.getUnlocalizedName());
 		GameRegistry.registerBlock(opal, opal.getUnlocalizedName());
-		
+
 		// Compressed blocks
 		GameRegistry.registerBlock(compressed_iron_block, compressed_iron_block.getUnlocalizedName());
 		GameRegistry.registerBlock(compressed_gold_block, compressed_gold_block.getUnlocalizedName());
@@ -131,11 +136,11 @@ public class Blocks extends net.minecraft.init.Blocks {
 		GameRegistry.registerBlock(compressed_ruby_block, compressed_ruby_block.getUnlocalizedName());
 		GameRegistry.registerBlock(compressed_sapphire_block, compressed_sapphire_block.getUnlocalizedName());
 		GameRegistry.registerBlock(compressed_diamond_block, compressed_diamond_block.getUnlocalizedName());
-		
+
 		// Blocks
 		GameRegistry.registerBlock(ruby_block, ruby_block.getUnlocalizedName());
 		GameRegistry.registerBlock(sapphire_block, sapphire_block.getUnlocalizedName());
-		
+
 		// Machines
 		GameRegistry.registerBlock(dark_matter_furnace, dark_matter_furnace.getUnlocalizedName());
 		GameRegistry.registerBlock(dark_matter_furnace_active, dark_matter_furnace_active.getUnlocalizedName());
