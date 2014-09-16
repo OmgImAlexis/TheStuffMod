a3b551c053fc2e043cbfa72cc723f4f17e96b3d8
Alexis Tyler <OmgImAlexis@gmail.com>
2014-09-16 17:28:14 +0930

    Moved items to your way

--- src/main/java/omgimalexis/allthethings/item/ModItems.java
+++ src/main/java/omgimalexis/allthethings/item/ModItems.java
@@ -12,155 +12,80 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModItems extends Items {
 
-	public static void mainRegistry() {
-		initialiseItem();
-		registerItem();
-	}
-
+	// Materials
 	public static ToolMaterial tutMaterial = EnumHelper.addToolMaterial("Tutorial Tool Material", 3, 200, 15.0F, 4.0F, 10);
-
 	public static ArmorMaterial tutArmorMaterial = EnumHelper.addArmorMaterial("Tutorial Armor Material", 33, new int[] { 2, 5, 4, 2 }, 10);
 
 	// Tools
-	public static Item ruby_pickaxe;
-	public static Item ruby_axe;
-	public static Item ruby_hoe;
-	public static Item ruby_spade;
-	public static Item sapphire_pickaxe;
-	public static Item sapphire_axe;
-	public static Item sapphire_hoe;
-	public static Item sapphire_spade;
-	
+	public static Item ruby_axe = new ItemAxe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":ruby_axe");
+	public static Item ruby_hoe = new ItemHoe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":ruby_hoe");
+	public static Item ruby_pickaxe = new ItemPickaxe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":ruby_pickaxe");
+	public static Item ruby_spade = new ItemSpade(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":ruby_spade");
+	public static Item sapphire_axe = new ItemAxe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":sapphire_axe");
+	public static Item sapphire_hoe = new ItemHoe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":sapphire_hoe");
+	public static Item sapphire_pickaxe = new ItemPickaxe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":sapphire_pickaxe");
+	public static Item sapphire_spade = new ItemSpade(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":sapphire_spade");
+
 	// Weapons
-	public static Item ruby_sword;
-	public static Item sapphire_sword;
+	
+	//Armor
+//		public static Item ruby_boots = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(ruby_boots.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":ruby_boots");
+//		public static Item ruby_helmet = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(ruby_helmet.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":ruby_helmet");
+//		public static Item ruby_pants = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(ruby_pants.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":ruby_pants");
+//		public static Item ruby_plate = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(ruby_plate.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":ruby_plate");
+//		public static Item sapphire_boots = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(sapphire_boots.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":sapphire_boots");
+//		public static Item sapphire_helmet = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(sapphire_helmet.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":sapphire_helmet");
+//		public static Item sapphire_pants = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(sapphire_pants.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":sapphire_pants");
+//		public static Item sapphire_plate = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(sapphire_plate.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":sapphire_plate");
 
-	// Armour
-	public static Item ruby_helmet;
-	public static Item ruby_plate;
-	public static Item ruby_pants;
-	public static Item ruby_boots;
-	public static Item sapphire_helmet;
-	public static Item sapphire_plate;
-	public static Item sapphire_pants;
-	public static Item sapphire_boots;
 
+	
 	// Ingots
-	public static Item ingotAdamant;
-	public static Item ingotBluerite;
-	public static Item ingotBronze;
-	public static Item ingotLunar;
-	public static Item ingotMithril;
-	public static Item ingotRunite;
-	public static Item ingotSilver;
-	public static Item ingotSteel;
-
+	public static Item ingotAdamant = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotAdamant");
+	public static Item ingotBluerite = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotBluerite");
+	public static Item ingotBronze = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotBronze");
+	public static Item ingotLunar = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotLunar");
+	public static Item ingotMithril = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotMithril");
+	public static Item ingotRunite = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotRunite");
+	public static Item ingotSilver = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotSilver");
+	public static Item ingotSteel = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotSteel");
+	
 	// Dusts
-	public static Item dustAdamanite;
-	public static Item dustBluerite;
-	public static Item dustBronze;
-	public static Item dustCoal;
-	public static Item dustCopper;
-	public static Item dustDaeyalt;
-	public static Item dustDiamond;
-	public static Item dustGold;
-	public static Item dustIron;
-	public static Item dustLunar;
-	public static Item dustMithril;
-	public static Item dustRuby;
-	public static Item dustRunite;
-	public static Item dustSapphire;
-	public static Item dustSilver;
-	public static Item dustSteel;
-	public static Item dustTin;
-
+	public static Item dustAdamanite = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustAdamanite");
+	public static Item dustBluerite = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustBluerite");
+	public static Item dustBronze = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustBronze");
+	public static Item dustCoal = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustCoal");
+	public static Item dustCopper = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustCopper");
+	public static Item dustDaeyalt = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustDaeyalt");
+	public static Item dustDiamond = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustDiamond");
+	public static Item dustGold = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustGold");
+	public static Item dustIron = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustIron");
+	public static Item dustLunar = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustLunar");
+	public static Item dustMithril = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustMithril");
+	public static Item dustRuby = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustRuby");
+	public static Item dustRunite = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustRunite");
+	public static Item dustSapphire = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustSapphire");
+	public static Item dustSilver = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustSilver");
+	public static Item dustSteel = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustSteel");
+	public static Item dustTin = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustTin");
+	
 	// Gems
-	public static Item gemAmber;
-	public static Item gemAmethyst;
-	public static Item gemCarnelian;
-	public static Item gemCitrine;
-	public static Item gemDiamond;
-	public static Item gemGarnet;
-	public static Item gemJade;
-	public static Item gemOnyx;
-	public static Item gemOpal;
-	public static Item gemQuartz;
-	public static Item gemRuby;
-	public static Item gemSapphire;
-	public static Item gemTopaz;
+	public static Item gemAmber = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemAmber");
+	public static Item gemAmethyst = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemAmethyst");
+	public static Item gemCarnelian = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemCarnelian");
+	public static Item gemCitrine = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemCitrine");
+	public static Item gemDiamond = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemDiamond");
+	public static Item gemGarnet = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemGarnet");
+	public static Item gemJade = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemJade");
+	public static Item gemOnyx = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemOnyx");
+	public static Item gemOpal = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemOpal");
+	public static Item gemQuartz = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemQuartz");
+	public static Item gemRuby = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemRuby");
+	public static Item gemSapphire = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemSapphire");
+	public static Item gemTopaz = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemTopaz");
 	
 	// This is how I do it
 	public static final ItemBasic fyrestone = new ItemBasic("fyrestone", TMCreativeTabs.misc);
-
-	public static void initialiseItem() {
-
-		// Tools
-		ruby_axe = new ItemAxe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":ruby_axe");
-		ruby_hoe = new ItemHoe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":ruby_hoe");
-		ruby_pickaxe = new ItemPickaxe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":ruby_pickaxe");
-		ruby_spade = new ItemSpade(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":ruby_spade");
-		sapphire_axe = new ItemAxe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":sapphire_axe");
-		sapphire_hoe = new ItemHoe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":sapphire_hoe");
-		sapphire_pickaxe = new ItemPickaxe(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":sapphire_pickaxe");
-		sapphire_spade = new ItemSpade(tutMaterial).setUnlocalizedName(Reference.MOD_ID + ":sapphire_spade");
-
-		// Weapons
-		
-		//Armor
-//		ruby_boots = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(ruby_boots.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":ruby_boots");
-//		ruby_helmet = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(ruby_helmet.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":ruby_helmet");
-//		ruby_pants = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(ruby_pants.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":ruby_pants");
-//		ruby_plate = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(ruby_plate.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":ruby_plate");
-//		sapphire_boots = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(sapphire_boots.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":sapphire_boots");
-//		sapphire_helmet = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(sapphire_helmet.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":sapphire_helmet");
-//		sapphire_pants = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(sapphire_pants.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":sapphire_pants");
-//		sapphire_plate = new ItemArmor(tutArmorMaterial, MainRegistry.proxy.addArmor(sapphire_plate.getUnlocalizedName()), 0).setUnlocalizedName(Reference.MOD_ID + ":sapphire_plate");
-
-		// Ingots
-		ingotAdamant = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotAdamant");
-		ingotBluerite = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotBluerite");
-		ingotBronze = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotBronze");
-		ingotLunar = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotLunar");
-		ingotMithril = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotMithril");
-		ingotRunite = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotRunite");
-		ingotSilver = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotSilver");
-		ingotSteel = new Item().setUnlocalizedName(Reference.MOD_ID + ":ingotSteel");
-		
-		// Dusts
-		dustAdamanite = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustAdamanite");
-		dustBluerite = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustBluerite");
-		dustBronze = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustBronze");
-		dustCoal = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustCoal");
-		dustCopper = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustCopper");
-		dustDaeyalt = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustDaeyalt");
-		dustDiamond = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustDiamond");
-		dustGold = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustGold");
-		dustIron = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustIron");
-		dustLunar = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustLunar");
-		dustMithril = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustMithril");
-		dustRuby = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustRuby");
-		dustRunite = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustRunite");
-		dustSapphire = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustSapphire");
-		dustSilver = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustSilver");
-		dustSteel = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustSteel");
-		dustTin = new Item().setUnlocalizedName(Reference.MOD_ID + ":dustTin");
-		
-		// Gems
-		gemAmber = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemAmber");
-		gemAmethyst = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemAmethyst");
-		gemCarnelian = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemCarnelian");
-		gemCitrine = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemCitrine");
-		gemDiamond = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemDiamond");
-		gemGarnet = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemGarnet");
-		gemJade = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemJade");
-		gemOnyx = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemOnyx");
-		gemOpal = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemOpal");
-		gemQuartz = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemQuartz");
-		gemRuby = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemRuby");
-		gemSapphire = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemSapphire");
-		gemTopaz = new Item().setUnlocalizedName(Reference.MOD_ID + ":gemTopaz");
-
-	}
 
 	public static void registerItem() {
 		
