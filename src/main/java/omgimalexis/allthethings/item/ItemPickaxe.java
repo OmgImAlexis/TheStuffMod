9aa5fd33ae95bf83ccedac728fdbd907fbe1e3fc
Shnupbups <shnupbups@gmail.com>
2014-09-16 21:03:10 +1000

    Removed unused and unneeded files.

--- src/main/java/omgimalexis/allthethings/item/ItemPickaxe.java
+++
@@ -1,9 +0,0 @@
-package omgimalexis.allthethings.item;
-
-public class ItemPickaxe extends net.minecraft.item.ItemPickaxe {
-
-	public ItemPickaxe(ToolMaterial material) {
-		super(material);
-	}
-
-}
