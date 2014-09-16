56becd96fde369f1791ecd25f56eb878e58729e2
Alexis Tyler <OmgImAlexis@gmail.com>
2014-09-16 21:25:41 +0930

    Start of pipe

--- src/main/java/omgimalexis/allthethings/init/ModTileEntities.java
+++ src/main/java/omgimalexis/allthethings/init/ModTileEntities.java
@@ -1,12 +1,14 @@
 package omgimalexis.allthethings.init;
 
 import omgimalexis.allthethings.tile_entity.TileEntityCompressor;
+import omgimalexis.allthethings.tile_entity.TileEntityPipe;
 import omgimalexis.allthethings.utility.LogHelper;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModTileEntities {
 	public static void init() {
 		GameRegistry.registerTileEntity(TileEntityCompressor.class, "Compressor");
+		GameRegistry.registerTileEntity(TileEntityPipe.class, "Pipe");
 		
 		LogHelper.info("Tile Entities initialised successfully!");
 	}
---
+++ src/main/java/omgimalexis/allthethings/tile_entity/render/TileEntityRenderPipe.java
@@ -0,0 +1,13 @@
+package omgimalexis.allthethings.tile_entity.render;
+
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityRenderPipe extends TileEntitySpecialRenderer {
+
+	@Override
+	public void renderTileEntityAt(TileEntity entity, double translationX, double translationY, double translationZ, float par5) {
+		System.out.println("asdasdasdasd");
+	}
+
+}
