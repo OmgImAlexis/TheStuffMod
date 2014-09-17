5499d9db3ec7767834be80b5a11351758db70eb0
Alexis Tyler <OmgImAlexis@gmail.com>
2014-09-17 18:48:01 +0930

    Textured? pipe?

--- src/main/java/omgimalexis/allthethings/tile_entity/render/TileEntityRenderPipe.java
+++ src/main/java/omgimalexis/allthethings/tile_entity/render/TileEntityRenderPipe.java
@@ -9,7 +9,7 @@ import omgimalexis.allthethings.lib.Reference;
 import org.lwjgl.opengl.GL11;
 
 public class TileEntityRenderPipe extends TileEntitySpecialRenderer {
-	ResourceLocation texture = new ResourceLocation(Reference.MOD_ID + "/textures/models/pipes/pipe.png");
+	ResourceLocation texture = new ResourceLocation(Reference.MOD_ID + ":textures/models/pipes/pipe.png");
 	
 	float pixel = 1F / 16F;
 	float texturePixel = 1F / 32F;
@@ -17,8 +17,11 @@ public class TileEntityRenderPipe extends TileEntitySpecialRenderer {
 	@Override
 	public void renderTileEntityAt(TileEntity tileEntity, double translationX, double translationY, double translationZ, float par5) {
 		GL11.glTranslated(translationX, translationY, translationZ);
+		GL11.glDisable(GL11.GL_LIGHTING);
 		this.bindTexture(texture);
 		drawCore(tileEntity);
+		GL11.glEnable(GL11.GL_LIGHTING);
+		GL11.glTranslated(-translationX, -translationY, -translationZ);
 	}
 
 	public void drawCore(TileEntity tileEntity) {
@@ -35,10 +38,25 @@ public class TileEntityRenderPipe extends TileEntitySpecialRenderer {
 			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 0*texturePixel, 0*texturePixel);
 			tessellator.addVertexWithUV(1-11*pixel/2, 11*pixel/2, 1-11*pixel/2, 0*texturePixel, 5*texturePixel);
 			
-			tessellator.addVertexWithUV(11*pixel/2, 11*pixel/2, 1-11*pixel/2, 0*texturePixel, 5*texturePixel);
-			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 0*texturePixel, 0*texturePixel);
-			tessellator.addVertexWithUV(1-11*pixel/2, 11*pixel/2, 1-11*pixel/2, 5*texturePixel, 5*texturePixel);
-			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 5*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 11*pixel/2, 11*pixel/2, 5*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 11*pixel/2, 5*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 11*pixel/2, 0*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 11*pixel/2, 0*texturePixel, 5*texturePixel);
+			
+			tessellator.addVertexWithUV(11*pixel/2, 11*pixel/2, 1-11*pixel/2, 5*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 5*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 11*pixel/2, 0*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 11*pixel/2, 0*texturePixel, 5*texturePixel);
+			
+			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 5*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 11*pixel/2, 5*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 11*pixel/2, 0*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1-1-11*pixel/2, 1-11*pixel/2, 0*texturePixel, 5*texturePixel);
+			
+			tessellator.addVertexWithUV(11*pixel/2, 1-1-11*pixel/2, 1-11*pixel/2, 5*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 11*pixel/2, 5*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 11*pixel/2, 0*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 0*texturePixel, 5*texturePixel);
 		}
 		tessellator.draw();
 	}
