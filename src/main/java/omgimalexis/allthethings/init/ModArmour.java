627b58d54a149b3440761937e7dadbf8b966c139
Shnupbups <shnupbups@gmail.com>
2014-09-18 19:30:08 +1000

    onyx armour, doesn't have a render texture yet.

--- src/main/java/omgimalexis/allthethings/init/ModArmour.java
+++ src/main/java/omgimalexis/allthethings/init/ModArmour.java
@@ -9,17 +9,26 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModArmour {
 	public static ArmorMaterial RUBY = EnumHelper.addArmorMaterial("RUBY", 20, new int[]{3, 7, 5, 2}, 13);
+	public static ArmorMaterial ONYX = EnumHelper.addArmorMaterial("ONYX", 45, new int[]{4, 9, 6, 4}, 17);
 	
 	public static final ItemBasicArmour helmetRuby = new ItemBasicArmour(RUBY, ClientProxy.addArmour("ruby"), 0, "rubyHelmet");
 	public static final ItemBasicArmour chestplateRuby = new ItemBasicArmour(RUBY, ClientProxy.addArmour("ruby"), 1, "rubyChestplate");
 	public static final ItemBasicArmour leggingsRuby = new ItemBasicArmour(RUBY, ClientProxy.addArmour("ruby"), 2, "rubyLeggings");
 	public static final ItemBasicArmour bootsRuby = new ItemBasicArmour(RUBY, ClientProxy.addArmour("ruby"), 3, "rubyBoots");
+	public static final ItemBasicArmour helmetOnyx = new ItemBasicArmour(ONYX, ClientProxy.addArmour("onyx"), 0, "onyxHelmet");
+	public static final ItemBasicArmour chestplateOnyx = new ItemBasicArmour(ONYX, ClientProxy.addArmour("onyx"), 1, "onyxChestplate");
+	public static final ItemBasicArmour leggingsOnyx = new ItemBasicArmour(ONYX, ClientProxy.addArmour("onyx"), 2, "onyxLeggings");
+	public static final ItemBasicArmour bootsOnyx = new ItemBasicArmour(ONYX, ClientProxy.addArmour("onyx"), 3, "onyxBoots");
 	
 	public static void init() {
 		GameRegistry.registerItem(helmetRuby, "rubyHelmet");
 		GameRegistry.registerItem(chestplateRuby, "rubyChestplate");
 		GameRegistry.registerItem(leggingsRuby, "rubyLeggings");
 		GameRegistry.registerItem(bootsRuby, "rubyBoots");
+		GameRegistry.registerItem(helmetOnyx, "onyxHelmet");
+		GameRegistry.registerItem(chestplateOnyx, "onyxChestplate");
+		GameRegistry.registerItem(leggingsOnyx, "onyxLeggings");
+		GameRegistry.registerItem(bootsOnyx, "onyxBoots");
 		
 		LogHelper.info("Armour initialized successfully!");
 	}
