1575c025a068aa54b80beb981da2be5844ccb605
Alexis Tyler <OmgImAlexis@gmail.com>
2014-09-18 18:11:28 +0930

    Working pipes

--- src/main/java/omgimalexis/allthethings/tile_entity/TileEntityPipe.java
+++ src/main/java/omgimalexis/allthethings/tile_entity/TileEntityPipe.java
@@ -2,10 +2,36 @@ package omgimalexis.allthethings.tile_entity;
 
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntityPipe extends TileEntity {
-	public TileEntity createNewTileEntity(World world, int par2) {
-		return new TileEntityPipe();
+	
+	/**
+	 * UP, DOWN, NORTH, EAST SOUTH WEST
+	 */
+	
+	public ForgeDirection[] connections = new ForgeDirection[6];
+	
+	public TileEntityPipe() {
+		
 	}
-
+	
+	public void updateEntity() {
+		this.updateConnections();
+	}
+	
+	public void updateConnections() {
+		if(this.worldObj.getTileEntity(xCoord, yCoord+1, zCoord) instanceof TileEntityPipe) connections[0] = ForgeDirection.UP;
+		else connections[0] = null;
+		if(this.worldObj.getTileEntity(xCoord, yCoord-1, zCoord) instanceof TileEntityPipe) connections[0] = ForgeDirection.DOWN;
+		else connections[1] = null;
+		if(this.worldObj.getTileEntity(xCoord, yCoord, zCoord-1) instanceof TileEntityPipe) connections[0] = ForgeDirection.NORTH;
+		else connections[2] = null;
+		if(this.worldObj.getTileEntity(xCoord+1, yCoord, zCoord) instanceof TileEntityPipe) connections[0] = ForgeDirection.EAST;
+		else connections[3] = null;
+		if(this.worldObj.getTileEntity(xCoord, yCoord, zCoord+1) instanceof TileEntityPipe) connections[0] = ForgeDirection.SOUTH;
+		else connections[4] = null;
+		if(this.worldObj.getTileEntity(xCoord-1, yCoord, zCoord) instanceof TileEntityPipe) connections[0] = ForgeDirection.WEST;
+		else connections[5] = null;
+	}
 }
--- src/main/java/omgimalexis/allthethings/tile_entity/render/TileEntityRenderPipe.java
+++ src/main/java/omgimalexis/allthethings/tile_entity/render/TileEntityRenderPipe.java
@@ -6,6 +6,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.util.ForgeDirection;
 import omgimalexis.allthethings.lib.Reference;
+import omgimalexis.allthethings.tile_entity.TileEntityPipe;
 
 import org.lwjgl.opengl.GL11;
 
@@ -21,26 +22,78 @@ public class TileEntityRenderPipe extends TileEntitySpecialRenderer {
 		GL11.glTranslated(translationX, translationY, translationZ);
 		GL11.glDisable(GL11.GL_LIGHTING);
 		this.bindTexture(texture);
-		drawCore(tileEntity);
-		drawConnection(ForgeDirection.UP);
+		{
+			drawCore(tileEntity);
+			
+			TileEntityPipe pipe = (TileEntityPipe) tileEntity;
+			for (int i = 0; i < pipe.connections.length; i++){
+				if (pipe.connections[i] != null) {
+					drawConnector(pipe.connections[i]);
+				}
+			}
+		}
+		drawConnector(ForgeDirection.EAST);
 		GL11.glEnable(GL11.GL_LIGHTING);
 		GL11.glTranslated(-translationX, -translationY, -translationZ);
 	}
 	
-	public void drawConnection(ForgeDirection direction) {
-		if (direction.equals(ForgeDirection.UP)) {
-			
-		}
-		
+	public void drawConnector(ForgeDirection direction) {
 		Tessellator tessellator = Tessellator.instance;
 		tessellator.startDrawingQuads();
 		{
-			tessellator.addVertexWithUV(1-11*pixel/2, 11*pixel/2, 1-11*pixel/2, 5*texturePixel, 5*texturePixel);
+			GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+			if (direction.equals(ForgeDirection.UP)) {
+				// ROTATE
+			} else if (direction.equals(ForgeDirection.DOWN)) {
+				GL11.glRotatef(180, 1, 0, 0);
+			} else if (direction.equals(ForgeDirection.SOUTH)) {
+				GL11.glRotatef(90, 1, 0, 0);
+			} else if (direction.equals(ForgeDirection.NORTH)) {
+				GL11.glRotatef(270, 1, 0, 0);
+			} else if (direction.equals(ForgeDirection.WEST)) {
+				GL11.glRotatef(90, 0, 0, 1);
+			} else if (direction.equals(ForgeDirection.EAST)) {
+				GL11.glRotatef(270, 0, 0, 1);
+			}
+			GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+			
+			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 5*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1, 1-11*pixel/2, 10*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1, 1-11*pixel/2, 10*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 5*texturePixel, 0*texturePixel);
+			
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 11*pixel/2, 5*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1, 11*pixel/2, 10*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1, 11*pixel/2, 10*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 11*pixel/2, 5*texturePixel, 0*texturePixel);
+			
+			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 11*pixel/2, 5*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1, 11*pixel/2, 10*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(1-11*pixel/2, 1, 1-11*pixel/2, 10*texturePixel, 0*texturePixel);
 			tessellator.addVertexWithUV(1-11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 5*texturePixel, 0*texturePixel);
-			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 0*texturePixel, 0*texturePixel);
-			tessellator.addVertexWithUV(11*pixel/2, 11*pixel/2, 1-11*pixel/2, 0*texturePixel, 5*texturePixel);
+			
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 1-11*pixel/2, 5*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1, 1-11*pixel/2, 10*texturePixel, 5*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1, 11*pixel/2, 10*texturePixel, 0*texturePixel);
+			tessellator.addVertexWithUV(11*pixel/2, 1-11*pixel/2, 11*pixel/2, 5*texturePixel, 0*texturePixel);
 		}
 		tessellator.draw();
+		
+		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+		if (direction.equals(ForgeDirection.UP)) {
+			// ROTATE
+		} else if (direction.equals(ForgeDirection.DOWN)) {
+			GL11.glRotatef(-180, 1, 0, 0);
+		} else if (direction.equals(ForgeDirection.SOUTH)) {
+			GL11.glRotatef(-90, 1, 0, 0);
+		} else if (direction.equals(ForgeDirection.NORTH)) {
+			GL11.glRotatef(-270, 1, 0, 0);
+		} else if (direction.equals(ForgeDirection.WEST)) {
+			GL11.glRotatef(-90, 0, 0, 1);
+		} else if (direction.equals(ForgeDirection.EAST)) {
+			GL11.glRotatef(-270, 0, 0, 1);
+		}
+		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
 	}
 
 	public void drawCore(TileEntity tileEntity) {
