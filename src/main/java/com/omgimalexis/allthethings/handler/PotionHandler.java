43cffb42bc55638ea3ae3c3caeafcefb56d337e5
Shnupbups <shnupbups@gmail.com>
2015-03-27 18:21:52 +1100

    Lessening works nao

--- src/main/java/com/omgimalexis/allthethings/handler/PotionHandler.java
+++ src/main/java/com/omgimalexis/allthethings/handler/PotionHandler.java
@@ -1,11 +1,8 @@
 package com.omgimalexis.allthethings.handler;
 
-import net.minecraft.client.particle.EntityFireworkOverlayFX;
-import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.MathHelper;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 
 import com.omgimalexis.allthethings.init.ModPotions;
@@ -30,8 +27,8 @@ public class PotionHandler {
 				event.entityLiving.worldObj.spawnParticle("splash", (event.entityLiving.posX+0.5F)-0.27000001072883606D, (event.entityLiving.posY+0.7F)+0.2199999988079071D, (event.entityLiving.posZ+0.5F), 0.0D, 0.0D, 0.0D);
 			}
 		} else if(event.entityLiving.isPotionActive(ModPotions.lessening)) {
-			if(event.entityLiving.getMaxHealth() > 0.1F) {
-				event.entityLiving.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(event.entityLiving.getMaxHealth()-0.1F);
+			if(event.entityLiving.getHealth() <= 1 || event.entityLiving.getEntityAttribute(SharedMonsterAttributes.maxHealth).getAttributeValue() <= 2.0) {
+				event.entityLiving.attackEntityFrom(new DamageSource("allthethings:lessening"), 9001);
 			}
 		}
 	}
--- src/main/java/com/omgimalexis/allthethings/potion/PotionLessening.java
+++ src/main/java/com/omgimalexis/allthethings/potion/PotionLessening.java
@@ -1,32 +1,46 @@
 package com.omgimalexis.allthethings.potion;
 
-import java.util.Iterator;
-import java.util.Map.Entry;
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.BaseAttributeMap;
-import net.minecraft.entity.ai.attributes.IAttribute;
-import net.minecraft.entity.ai.attributes.IAttributeInstance;
 
 public class PotionLessening extends PotionBasic{
-
+	
+	LinkedList<AttributeModifier> modifiers = new LinkedList<AttributeModifier>();
+	
 	public PotionLessening(int par1, boolean par2, int par3) {
 		super(par1, par2, par3);
-		// TODO Auto-generated constructor stub
 	}
 	
-	public void applyAttributesModifiersToEntity(EntityLivingBase p_111185_1_, BaseAttributeMap p_111185_2_, int p_111185_3_)
-    {
-       super.applyAttributesModifiersToEntity(p_111185_1_, p_111185_2_, p_111185_3_);
-       p_111185_1_.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(p_111185_1_.getMaxHealth()-(0.1F*this.getEffectiveness()));
+	public boolean isReady(int p_76397_1_, int p_76397_2_){
+        return true;
     }
 	
-	public void removeAttributesModifiersFromEntity(EntityLivingBase p_111187_1_, BaseAttributeMap p_111187_2_, int p_111187_3_){
-        super.removeAttributesModifiersFromEntity(p_111187_1_, p_111187_2_, p_111187_3_);
-
-        p_111187_1_.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(20);
+	public void performEffect(EntityLivingBase p_76394_1_, int p_76394_2_) {
+		applyAttributesModifiersToEntity(p_76394_1_, p_76394_1_.getAttributeMap(), p_76394_2_);
+	}
+	
+	@Override
+	public void applyAttributesModifiersToEntity(EntityLivingBase entity, BaseAttributeMap map, int p_111185_3_) {
+		AttributeModifier modifier = new AttributeModifier("lessen", -0.1D*this.getEffectiveness(), 0);
+		entity.getEntityAttribute(SharedMonsterAttributes.maxHealth).applyModifier(modifier);
+		modifiers.add(modifier);
+		if(new Random().nextInt(20) <= 2) entity.worldObj.playSoundAtEntity(entity, "minecraft:mob.zombiepig.zpigdeath", 1.0F, 0.7F);
+		if(entity.getHealth() > entity.getMaxHealth()) entity.setHealth(entity.getMaxHealth());
+		super.applyAttributesModifiersToEntity(entity, map, p_111185_3_);
+	}
+	
+	public void removeAttributesModifiersFromEntity(EntityLivingBase entity, BaseAttributeMap p_111187_2_, int p_111187_3_){
+		for(int i = 0; i < modifiers.size(); i++) {
+			entity.getEntityAttribute(SharedMonsterAttributes.maxHealth).removeModifier(modifiers.get(i));
+		}
+		super.removeAttributesModifiersFromEntity(entity, p_111187_2_, p_111187_3_);
     }
 
 }
